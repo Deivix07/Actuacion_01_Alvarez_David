afc870f619d4e4ff219a195bc81178b09ad9a8d8
Deivix07 <davidalvarezdm1226@gmail.com>
2025-06-06 19:53:07 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,15 @@ cards.forEach(card => {
     card.style.transform = "scale(1)";
   });
 });
+
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+  link.addEventListener('click', () => {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse.classList.contains('show')) {
+      const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: true
+      });
+      bsCollapse.hide();
+    }
+  });
+});
